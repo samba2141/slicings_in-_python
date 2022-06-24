2a2faf22386a1bd90ebd51b73990ef181c9823b0
samba2141 <107167681+samba2141@users.noreply.github.com>
2022-06-24 13:58:36 +0530

    Add files via upload

---
+++ slicing_part1.py
@@ -0,0 +1,18 @@
+a='programming'
+#print(a[:1])#p
+#print(a[:2])#pr
+# print(a[:3])#pro
+# print(a[:4])#prog
+# print(a[:5])#progr
+# print(a[:6])#progr
+# print(a[:7])#progra
+# print(a[:8])#program
+print(a[1:9])#rogramm
+print(a[2:10])#ogrammin
+# print(a[:11])
+# print(a[:12])
+#print(a[1:1])
+# print(a[1:2])
+# print(a[1:3])#1<2
+# print(a[1:4])#1<3
+# print(a[1:5])#1<4
---
+++ sliicing_part2.py
@@ -0,0 +1,18 @@
+a='python'
+print(a[-5:]) #ython
+print(a[-2:])#on
+print(a[-3:-1])#ho
+print(a[-6:-2])#pyth
+print(a[-1:-3])#blankline
+print(a[-6:])#python
+print(a[-5:])#ython
+print(a[-99:])#python
+print(a[-2:])#on
+print(a[-1:])#n
+print(a[-7:])#python
+print(a[ : :-1])#nohtyp
+print(a[ : :-2])#nhy
+print(a[ : :-3])#nt
+print(a[ : :-4])#ny
+print(a[ : :-5])#np
+print(a[ : :-6])#n
